995ad7ec98ea91b2f0bcef93ba7eac5766fcc085
Farbod Najafi <farbodnajafi@gmail.com>
2020-08-17 15:27:41 +0430

    Replaced post's functional list_view with Classed based ListView

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 from .models import Post
 
 
-def post_list(request):
-    object_list = Post.published.all()
-    paginator = Paginator(object_list, 3)  # 3 post in each page
-    page = request.GET.get('page')
-
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an integer deliver the first page
-        posts = paginator.page(1)
-    except EmptyPage:
-        # If page is out if range deliver last page of results
-        posts = paginator.page(paginator.num_pages)
-
-    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
 def post_detail(request, year, month, day, post):
