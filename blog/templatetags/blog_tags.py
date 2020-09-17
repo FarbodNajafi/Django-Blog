65ac8ccc2ed675daab9c23287c672873b6d9f24d
Farbod Najafi <farbodnajafi@gmail.com>
2020-09-17 14:43:01 +0430

    Added markdown filter function

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,8 @@
+import markdown
+
 from django import template
 from django.db.models import Count
+from django.utils.safestring import mark_safe
 
 from ..models import Post
 
@@ -22,3 +25,8 @@ def get_most_commented_posts(count=5):
     return Post.published.annotate(
         total_posts=Count('comments')
     ).order_by('-total_comments')[:count]
+
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
