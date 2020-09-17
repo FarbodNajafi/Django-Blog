466b7dec821c16891b25ed1ff49ac27998585c0e
Farbod Najafi <farbodnajafi@gmail.com>
2020-09-18 03:39:54 +0430

    Fixed the bug where total_comment field was named incorrectly in get_most_commented_posts tag

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -23,7 +23,7 @@ def show_latest_posts(count=5):
 @register.simple_tag
 def get_most_commented_posts(count=5):
     return Post.published.annotate(
-        total_posts=Count('comments')
+        total_comments=Count('comments')
     ).order_by('-total_comments')[:count]
 
 
