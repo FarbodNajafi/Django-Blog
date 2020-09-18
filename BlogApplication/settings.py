3e40d96ebe455a1d21febc8f18f9194f1868d6cd
Farbod Najafi <farbodnajafi@gmail.com>
2020-09-19 04:08:26 +0430

    Added postgres configuration

--- BlogApplication/settings.py
+++ BlogApplication/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django.contrib.sitemaps',
+    'django.contrib.postgres',
     'blog.apps.BlogConfig',
     'taggit',
 ]
@@ -78,8 +79,10 @@ WSGI_APPLICATION = 'BlogApplication.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'django',
+        'PASSWORD': '****',
     }
 }
 
