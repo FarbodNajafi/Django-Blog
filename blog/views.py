cbb0a6e9d3af1110d45e7b08cdd0b80ad3a624f3
Farbod Najafi <farbodnajafi@gmail.com>
2020-09-19 04:23:18 +0430

    Replaced Weighting queries with TrigramSimilarity search approach

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import TrigramSimilarity
 from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Count
@@ -120,14 +120,23 @@ def post_search(request):
 
         if form.is_valid():
             query = form.cleaned_data['query']
-            search_vector = SearchVector('title', weight='A') + \
-                            SearchVector('body', weight='B')
-            search_query = SearchQuery(query)
+
+            # Weighting queries
+
+            # search_vector = SearchVector('title', weight='A') + \
+            #                 SearchVector('body', weight='B')
+            # search_query = SearchQuery(query)
+            #
+            # results = Post.published.annotate(
+            #     search=search_vector,
+            #     rank=SearchRank(search_vector, search_query),
+            # ).filter(rank__gte=0.3).order_by('-rank')
+
+            # TrigramSimilarity
 
             results = Post.published.annotate(
-                search=search_vector,
-                rank=SearchRank(search_vector, search_query),
-            ).filter(rank__gte=0.3).order_by('-rank')
+                similarity=TrigramSimilarity('title', query)
+            ).filter(similarity__gte=0.1).order_by('-similarity')
 
     return render(request,
                   'blog/post/search.html',
